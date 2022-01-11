d30feef8e618954bfe344ea272310f4beefb949b
Daniel Guiomarino <danielbrenom@gmail.com>
2022-01-11 19:13:09 -0300

    - Changing execution mechanic to parallelism

--- Bankrupt/Program.cs
+++ Bankrupt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Bankrupt.Architecture;
@@ -12,29 +13,24 @@ namespace Bankrupt
     {
         private const int SimulationAmount = 300;
         private static readonly int SimulationShardAmount = 5;
-        
+
         private static void Main(string[] args)
         {
             var results = new List<GameResult>();
-            var shards = new List<Task>();
             Console.WriteLine("Simulation start");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             if (SimulationAmount % SimulationShardAmount != 0)
                 throw new ArgumentOutOfRangeException(nameof(SimulationShardAmount), "Cannot divide simulation into shards, simulation amount must be divisible by shard amount");
-            for (var i = 0; i < SimulationShardAmount; i++)
+            Parallel.For(0, SimulationAmount * SimulationShardAmount, shardIndex =>
             {
-                shards.Add(Task.Factory.StartNew(() =>
-                {
-                    for (var j = 0; j < SimulationAmount/SimulationShardAmount; j++)
-                    {
-                        var game = new GameLogic();
-                        var result = game.RunGame();
-                        results.Add(new GameResult{TurnCount = result.TurnCount, WinnerStrategy = result.Winner.Strategy});
-                    }
-                }));
-            }
-            Task.WaitAll(shards.ToArray());
+                var game = new GameLogic();
+                var result = game.RunGame();
+                results.Add(new GameResult { TurnCount = result.TurnCount, WinnerStrategy = result.Winner.Strategy });
+            });
+            stopwatch.Stop();
             AnalyzeResults(results);
-            Console.WriteLine("Simulation end");
+            Console.WriteLine("Simulation end in {0} millisseconds", stopwatch.ElapsedMilliseconds);
         }
 
         private static void AnalyzeResults(IReadOnlyCollection<GameResult> results)
@@ -46,10 +42,10 @@ namespace Bankrupt
                                          .First();
             Console.WriteLine($"Games ended by timeout: {results.Count(r => r.TurnCount > 999)}");
             Console.WriteLine($"Turn count average: {(int)results.Average(r => r.TurnCount)}");
-            Console.WriteLine($"{PlayerStrategy.Cautious.ToString()} win percentage: {(double)results.Count(r => r.WinnerStrategy == PlayerStrategy.Cautious)/results.Count*100} %");
-            Console.WriteLine($"{PlayerStrategy.Demanding.ToString()} win percentage: {(double)results.Count(r => r.WinnerStrategy == PlayerStrategy.Demanding)/results.Count*100} %");
-            Console.WriteLine($"{PlayerStrategy.Impulsive.ToString()} win percentage: {(double)results.Count(r => r.WinnerStrategy == PlayerStrategy.Impulsive)/results.Count*100} %");
-            Console.WriteLine($"{PlayerStrategy.Random.ToString()} win percentage: {(double)results.Count(r => r.WinnerStrategy == PlayerStrategy.Random)/results.Count*100} %");
+            Console.WriteLine($"{PlayerStrategy.Cautious.ToString()} win percentage: {(double)results.Count(r => r.WinnerStrategy == PlayerStrategy.Cautious) / results.Count * 100} %");
+            Console.WriteLine($"{PlayerStrategy.Demanding.ToString()} win percentage: {(double)results.Count(r => r.WinnerStrategy == PlayerStrategy.Demanding) / results.Count * 100} %");
+            Console.WriteLine($"{PlayerStrategy.Impulsive.ToString()} win percentage: {(double)results.Count(r => r.WinnerStrategy == PlayerStrategy.Impulsive) / results.Count * 100} %");
+            Console.WriteLine($"{PlayerStrategy.Random.ToString()} win percentage: {(double)results.Count(r => r.WinnerStrategy == PlayerStrategy.Random) / results.Count * 100} %");
             Console.WriteLine($"Strategy with most wins: {mostWinStrategy.ToString()}");
         }
     }
